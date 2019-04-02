06d884a444040dca40a3b5ca921a0bdb11689a4f
gepopp <office@sewemo.eu>
2019-04-02 17:50:28 +0200

    finished nl form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -96,43 +96,23 @@
                     Ein exklusives Netzwerk für alle die in der Branche was zu sagen haben.
                 </div>
 
-                <form class="form-inline justify-content-center">
+                <form class="form-inline justify-content-center js-cm-form" id="subForm" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="30FEA77E7D0A9B8D7616376B90063231559560C2B49BDB229227B6C69FC82CE3F1BAFDD3C8279665E23F4FA100BF04A56905C95659DC77BCA4E1EFF287F689FD">
                     <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
                     <div class="input-group mb-2 mr-sm-2">
                         <div class="input-group-prepend">
                             <div class="input-group-text bg-transparent border-0">@</div>
                         </div>
-                        <input type="email" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Username">
+                        <input id="fieldEmail" name="cm-efhti-efhti" type="email" class="form-control js-cm-email-input" id="inlineFormInputGroupUsername2" placeholder="e-mail adresse" required>
                     </div>
-
                     <div class="form-check mb-2 mr-sm-2">
-                        <input class="form-check-input" type="checkbox" id="inlineFormCheck">
+                        <input id="cm-privacy-consent" name="cm-privacy-consent" required type="checkbox" />
                         <label class="form-check-label" for="inlineFormCheck">
-                            Remember me
+                            Ja, ich will hello.ir Mails erhalten.
                         </label>
                     </div>
-
-                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
-                </form>
-
-
-                <form id="subForm" class="js-cm-form" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="30FEA77E7D0A9B8D7616376B90063231559560C2B49BDB229227B6C69FC82CE3F1BAFDD3C8279665E23F4FA100BF04A56905C95659DC77BCA4E1EFF287F689FD">
-                    <div class="form-group">
-                        <label for="fieldEmail">E-Mail</label>
-                        <input id="fieldEmail" name="cm-efhti-efhti" type="email" class="js-cm-email-input form-control border-0 border-bottom border-primary" required />
-                    </div>
-
-
-                    <div>
-                        <input id="cm-privacy-consent" name="cm-privacy-consent" required type="checkbox"
-                        />
-                        <label for="cm-privacy-consent">Ja, ich will hello.ir Mails erhalten.</label>
-                        <input id="cm-privacy-consent-hidden" name="cm-privacy-consent-hidden"
-                               type="hidden" value="true" />
-                    </div>
-                    <p>
-                        <button class="js-cm-submit-button btn btn-light" type="submit">anmelden</button>
-                    </p>
+                    <input id="cm-privacy-consent-hidden" name="cm-privacy-consent-hidden"
+                           type="hidden" value="true" />
+                    <button class="js-cm-submit-button btn btn-light btn-sm" type="submit">anmelden</button>
                 </form>
                 <script type="text/javascript" src="https://js.createsend1.com/javascript/copypastesubscribeformlogic.js"></script>
             </div>
